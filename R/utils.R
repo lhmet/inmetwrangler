2cc2ab349de12dd433d9c0f855785d18179d67df
lhmet <lhmet@ufsm.br>
2017-09-26 11:26:37 -0300

    remove code globalVariables ...

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,3 @@
-# utils::globalVariables(c(".", ".only.problems", "trash", 
-#                          "tens_bat","prec", "month",
-#                          "day", "cld_tot", "hour_utc",
-#                          "site", "visib", "year"))
 
 . <- .only.problems <- trash <- tens_bat <- prec <- month <- day <- 
   cld_tot <- hour_utc <- site <- visib <- year <- NULL
