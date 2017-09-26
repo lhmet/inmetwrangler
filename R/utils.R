17fbaf558e233a58634439451d34e4410824ffa5
lhmet <lhmet@ufsm.br>
2017-09-25 22:32:07 -0300

    fix things

---
+++ R/utils.R
@@ -0,0 +1,17 @@
+utils::globalVariables(c(".", ".only.problems", "trash", "codigo", 
+                         "tens_bat","prec", "month",
+                         "day", "cld_tot", "hour_utc",
+                         "site", "visib", "year"))
+
+
+#' Pipe operator
+#'
+#' See \code{\link[dplyr]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom dplyr %>%
+#' @usage lhs \%>\% rhs
+NULL
