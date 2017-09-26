13b54c7d4f450a3734949c3e506d93214e0b3a04
lhmet <lhmet@ufsm.br>
2017-09-26 09:46:38 -0300

    remove pipe

--- R/utils.R
+++ R/utils.R
@@ -5,15 +5,3 @@
 
 . <- .only.problems <- trash <- tens_bat <- prec <- month <- day <- 
   cld_tot <- hour_utc <- site <- visib <- year <- NULL
-
-#' Pipe operator
-#'
-#' See \code{\link[dplyr]{\%>\%}} for more details.
-#'
-#' @name %>%
-#' @rdname pipe
-#' @keywords internal
-#' @export
-#' @importFrom dplyr %>%
-#' @usage lhs \%>\% rhs
-NULL
