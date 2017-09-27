0609aabf8b9a41b7312d1f61cfbd31c0b47fbfd7
lhmet <lhmet@ufsm.br>
2017-09-27 16:26:50 -0300

    add more vars

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,4 @@
 
 . <- .only.problems <- trash <- tens_bat <- prec <- month <- day <- 
-  cld_tot <- hour_utc <- site <- visib <- year <- .verbose <- NULL
+  cld_tot <- hour_utc <- site <- visib <- year <- .verbose <- row <- 
+  expected <- actual <- NULL
